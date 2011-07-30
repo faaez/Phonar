30df61decff0561133e461a84973a2799f3efa3a
Jorge Lugo <jlugo@princeton.edu>
2011-07-30 16:21:51 -0700

    Added the receiver and updated AndroidManifest
    Basics for C2DM junk.

--- gen/com/phonarapp/client/R.java
+++ gen/com/phonarapp/client/R.java
@@ -18,6 +18,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int register=0x7f040000;
+        public static final int unregister=0x7f040002;
     }
 }
--- src/com/phonarapp/client/Phonar.java
+++ src/com/phonarapp/client/Phonar.java
@@ -1,6 +1,8 @@
 package com.phonarapp.client;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Phonar extends Activity {
@@ -9,5 +11,13 @@ public class Phonar extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // C2DM
+        Intent registrationIntent =
+        		new Intent("com.google.android.c2dm.intent.REGISTER");
+        registrationIntent.putExtra("app", PendingIntent.getBroadcast(
+        		this, 0, new Intent(), 0)); // boilerplate
+        registrationIntent.putExtra("sender", "jeffreyhodes@gmail.com");
+        startService(registrationIntent);
     }
 }
