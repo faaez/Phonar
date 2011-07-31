86a96f53a0ad7b6deb8705f4a08c0f8673b53797
Jorge Lugo <jlugo@princeton.edu>
2011-07-30 17:03:27 -0700

    Add two buttons to register and unregister
    "lol phonar" registers it with Google and our server. "wtf phonar"
    unregisters it from google.
    
    Also added *.class to .gitignore

--- src/com/phonarapp/client/Phonar.java
+++ src/com/phonarapp/client/Phonar.java
@@ -2,22 +2,44 @@ package com.phonarapp.client;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Phonar extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        // C2DM
-        Intent registrationIntent =
-        		new Intent("com.google.android.c2dm.intent.REGISTER");
-        registrationIntent.putExtra("app", PendingIntent.getBroadcast(
-        		this, 0, new Intent(), 0)); // boilerplate
-        registrationIntent.putExtra("sender", "jeffreyhodes@gmail.com");
-        startService(registrationIntent);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// setup C2DM
+		final Context context = this;
+		Button registerButton = (Button) findViewById(R.id.register_button);
+		registerButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent registrationIntent = new Intent(
+						"com.google.android.c2dm.intent.REGISTER");
+				registrationIntent.putExtra("app", PendingIntent
+						.getBroadcast(context, 0, new Intent(), 0));
+				registrationIntent.putExtra("sender",
+						"jeffreyhodes@gmail.com");
+				startService(registrationIntent);
+			}
+		});
+		Button unregisterButton = (Button) findViewById(R.id.unregister_button);
+		unregisterButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent unregIntent =
+						new Intent("com.google.android.c2dm.intent.UNREGISTER");
+				unregIntent.putExtra("app", PendingIntent
+						.getBroadcast(context, 0, new Intent(), 0));
+				startService(unregIntent);
+			}
+		});
+	}
 }
